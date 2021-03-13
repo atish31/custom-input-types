49548ae8f837627f9615d4e7e4e6e42f5e5979d0
shravan20 <mrshravankumarb@gmail.com>
2021-03-14 01:55:25 +0530

    [ :heavy_check_mark: custom-numerics/custom-numerics.js ] test | Added Unit Tests for telephone() | 1 passing tests

--- specs/custom-numerics/custom-numerics.spec.js
+++ specs/custom-numerics/custom-numerics.spec.js
@@ -1,4 +1,4 @@
-import { customNumber as _customNumber } from '../../modules/custom-numerics/custom-numerics';
+import { customNumber as _customNumber, telephone as _telephone } from '../../modules/custom-numerics/custom-numerics';
 import { expect } from 'chai';
 
 
@@ -17,6 +17,22 @@ describe('Unit Testing for #customNumber()', () => {
     let testData = _customNumber('54321QWERTY');
     expect(testData).to.be.equal('54321');
     expect(testData).to.not.equal('54321QWERTY');
+
+  });
+
+});
+
+describe('Unit Testing for #telephone()', () => {
+
+  it('Given a number string as input, should return the number as it is', () => {
+
+    let e = {
+      target: {
+        value: "9876543210"
+      }
+    };
+    let testData = _telephone(e);
+    console.log(testData);
 
   });
 
