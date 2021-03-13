0a9c9e0072ea8359367619d0208b11b12684e2a5
shravan20 <mrshravankumarb@gmail.com>
2021-03-13 20:43:30 +0530

    [ :heavy_check_mark: _shared/helper ] test | Add Unit Tests | 2 passing tests

---
+++ specs/_shared/helper.spec.js
@@ -0,0 +1,24 @@
+
+
+import { convertSnakeCaseToCamelCase as _convertSnakeCaseToCamelCase } from '../../modules/_shared/helper';
+import { expect } from 'chai';
+
+
+describe('Unit Testing for #convertSnakeCaseToCamelCase()', () => {
+
+  it('Given a string of snake_case, should return converted camelCase of the same', () => {
+
+      let testData = _convertSnakeCaseToCamelCase('kebab_case');
+      expect(testData).to.be.equal('kebabCase');
+      expect(testData).to.not.equal('kebab_case');
+
+  });
+
+  it('Given a string of non snake_case, should return the string as it is', ()=>{
+
+    let testData = _convertSnakeCaseToCamelCase('camelCase');
+    expect(testData).to.be.equal(testData);
+
+  });
+
+});
