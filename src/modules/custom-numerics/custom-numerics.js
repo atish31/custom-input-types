8ac17e44b3270b28375ac6c71fc2d434c7bb234d
ShubhKotnala <skshubhankar@gmail.com>
2021-03-18 23:44:41 +0530

    [hotfix] (custom-numerics) | Fixed 'phone' input type

--- src/modules/custom-numerics/custom-numerics.js
+++ src/modules/custom-numerics/custom-numerics.js
@@ -3,11 +3,9 @@ export function customNumber(value) {
 }
 
 export function telephone(e) {
-  e.target.value = e.target.value.replace(/\D/g, "").substring(0, 10);
-  const phone = e.target.value;
-  let match = phone.match(/^(\d{3})(\d{3})(\d{4})$/);
-  if (match) {
-    e.target.value = "(" + match[1] + ") " + match[2] + "-" + match[3];
+  e.target.value = e.target.value.replace(/\D+/g, "").substring(0,10);
+  if(e.target.value.charAt(0) >= '0' && e.target.value.charAt(0) <=5){
+    e.target.value = '';
   }
 }
 
